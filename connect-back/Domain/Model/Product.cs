2232dde11972d5ba1de8ae07ee70a430589c07a7
ece-diego <ece.diegopg@gmail.com>
2020-10-21 18:20:53 -0500

    cambiado el precio a decimal

--- connect-back/Domain/Model/Product.cs
+++ connect-back/Domain/Model/Product.cs
@@ -5,6 +5,6 @@ namespace connect_back.Domain.Model{
         public string Code {get;set;}
         public string Name {get;set;}
         public string Description {get;set;}
-        public int Price{get;set;}
+        public float Price{get;set;}
     }
 }
