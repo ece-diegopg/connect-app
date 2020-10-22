c54462f3be4f8e88058fb87edfac73cb7036173e
ece-diego <ece.diegopg@gmail.com>
2020-10-21 23:57:41 -0500

    ProductController creado

---
+++ connect-back/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using connect_back.Domain.Services;
+using connect_back.Dto;
+using connect_back.Mapper;
+
+namespace connect_back.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet("{productId}")]
+        [ProducesResponseType(typeof(ProductDTO), 200)]
+        public async Task<IActionResult> GetProductByIdAsync(int productId)
+        {
+            if(typeof(int) == productId.GetType()){
+                return BadRequest();
+            }
+            var result = await _productService.GetProductByIdAsync(productId);
+            if(!result.Success){
+                return BadRequest(result.Message);
+            }
+            return Ok(result);
+        }
+    }
+}
